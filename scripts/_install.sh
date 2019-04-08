fd06f01ca46cdd7ddae714f8f0bf036560769a29
Thinker <mstr.raphael@gmail.com>
2019-04-08 21:36:22 +0800

    modify the script for debugging

--- scripts/_install.sh
+++ scripts/_install.sh
@@ -5,7 +5,7 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "dark" ] && exit 0
 
-git clone -b master --depth 5 https://${GITHUB_PAT}@github.com/${DEPLOYMENT_REPO}.git build
+git clone -b fix_ssl_error --depth 5 https://${GITHUB_PAT}@github.com/${DEPLOYMENT_REPO}.git build
 cd build
 
 pipenv install
