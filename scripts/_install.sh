55ec112220c01be524e36bc0d309a0f0cd767e6b
Thinker <mstr.raphael@gmail.com>
2019-04-10 18:04:31 +0800

    test the mongodb branch

--- scripts/_install.sh
+++ scripts/_install.sh
@@ -3,9 +3,9 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "dark" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "dark-18" ] && exit 0
 
-git clone -b master --depth 5 https://${GITHUB_PAT}@github.com/${DEPLOYMENT_REPO}.git build
+git clone -b mongodb --depth 5 https://${GITHUB_PAT}@github.com/${DEPLOYMENT_REPO}.git build
 cd build
 
 pipenv install --pypi-mirror https://pypi.python.org/simple
