283981e0b3273691998c03537a02e69946aadf3c
Thinker <mstr.raphael@gmail.com>
2019-04-11 15:12:44 +0800

    add another branch for testing

--- scripts/_daily.sh
+++ scripts/_daily.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "dark-14" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "dark-7" ] && exit 0
 
 cd build
 pipenv run invoke wrangler.search -t cn -l 200
