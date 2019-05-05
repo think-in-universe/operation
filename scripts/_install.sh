e86cdf4220901e564f4e6ca4fbd8e7276228e71d
Thinker <mstr.raphael@gmail.com>
2019-05-05 12:00:55 +0800

    test a new branch

--- scripts/_install.sh
+++ scripts/_install.sh
@@ -5,7 +5,7 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "debug" ] && exit 0
 
-git clone -b en-6 --depth 5 https://${GITHUB_PAT}@github.com/${DEPLOYMENT_REPO}.git build
+git clone -b en-8 --depth 5 https://${GITHUB_PAT}@github.com/${DEPLOYMENT_REPO}.git build
 cd build
 
 pipenv install --pypi-mirror https://pypi.python.org/simple
