ebdec43e7fc1b7761269ce564770df562a8c785a
Thinker <mstr.raphael@gmail.com>
2019-05-07 15:10:31 +0800

    enable a new branch

--- scripts/_install.sh
+++ scripts/_install.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "dark-16" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "dark-15" ] && exit 0
 
 git clone -b master --depth 5 https://${GITHUB_PAT}@github.com/${DEPLOYMENT_REPO}.git build
 cd build
