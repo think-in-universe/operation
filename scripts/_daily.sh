8b77393fad64feddd16dabce8be34ab4f24354b6
Thinker <mstr.raphael@gmail.com>
2019-05-08 12:37:22 +0800

    add a new branch

--- scripts/_daily.sh
+++ scripts/_daily.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "dark-19" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "dark-22" ] && exit 0
 
 cd build
 pipenv run invoke wrangler.wander
